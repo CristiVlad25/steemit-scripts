6090d30724a8a16f800990fa2303023680c9f94c
Cristi <CristiVlad25@users.noreply.github.com>
2016-09-13 08:24:50 +0300

    Update steemlytics.py

--- steemlytics.py
+++ steemlytics.py
@@ -5,21 +5,25 @@ rpc = SteemNodeRPC('ws://node.steem.ws')
 from collections import Counter
 import csv
 
-# August 30 - beginning of the day - block 4517262
-# September 5 - end of the day - block 4718263
+# September 6 - beginning of the day - block 4718264
+# September 12 - end of the day - block 4919136
 
 voters = ['blocktrades', 'jamesc', 'smooth', 'dantheman', 'tombstone', 'summon',
             'steemed', 'rainman', 'wang', 'complexring', 'riverhead', 'roadscape',
             'nextgencrypto', 'silversteem', 'donkeypong', 'proskynneo', 'blackjack',
-            'firstclass', 'enki', 'clayop', 'wackou', 'steemit200','kushed', 'xeldal',
-            'arhag','fuzzyvest','pharesim','steempty', 'bitcube','onceuponatime','witnes.svk'
-            ,'abit','berniesanders']
+            'firstclass', 'enki', 'clayop', 'wackou', 'steemit200', 'kushed', 'xeldal',
+            'arhag', 'fuzzyvest', 'pharesim', 'steempty', 'bitcube', 'onceuponatime', 'witnes.svk',
+            'abit', 'berniesanders', 'ned', 'dan', 'val-a', 'itsascam', 'skywalker', 'smooth.witness',
+            'datasecuritynode', 'au1nethyb1', 'bitcoin2016', 'analisa', 'paladin', 'satoshifund', 'alphabet',
+            'excalibur', 'recursive', 'lafona-miner', 'badassmother', 'xeroc']
 
 voterlst = []
 authorlst = []
 permlinklst = []
 
-for i in range(4517262, 4718263):         # parsing the blocks between Aug 29 - Sept 5 (end of day)
+# parsing the blocks between Sept 6 - Sept 12 (end of day)
+
+for i in range(4718264, 4919136):
     dys = rpc.get_block(i)['transactions']
     for tx in dys:
         for operation in tx['operations']:
@@ -27,7 +31,11 @@ for i in range(4517262, 4718263):         # parsing the blocks between Aug 29 - 
                 voter = operation[1]['voter']
                 author = operation[1]['author']
                 permlink = operation[1]['permlink']
-                if permlink[:3] != "re-":      # we're looking for votes on posts, not on comments
+
+
+# we're looking for votes on posts, not on comments
+
+                if permlink[:3] != "re-":
                     voterlst.append(voter)
                     authorlst.append(author)
 
@@ -36,7 +44,7 @@ authorCounts = Counter(authorlst)
 
 # indexing results for voters (name + votes given)
 
-writefileV = open('voterresults.csv', 'w', newline='')
+writefileV = open('voterresults4.csv', 'w', newline='')
 writerV = csv.writer(writefileV)
 
 for vkey, vcount in voteCounts.items():
@@ -44,7 +52,7 @@ for vkey, vcount in voteCounts.items():
 
 # indexing results for authors (name + votes received)
 
-writefileA = open('authorresults.csv', 'w', newline='')
+writefileA = open('authorresults4.csv', 'w', newline='')
 writerA = csv.writer(writefileA)
 
 for akey, acount in authorCounts.items():
